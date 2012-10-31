8fbcf3692bf1db2095bf8fc40bf05de40e0d5222
Niklas Een <niklas@een.se>
2012-10-30 21:07:38 -0700

    Forgot...

---
+++ EXTERN/abcSat/compile.sh
@@ -0,0 +1,8 @@
+gcc arch_flags.c -o arch_flags
+AFLAGS=`arch_flags`
+
+g++ $AFLAGS -I. "$@" -c satSolver.cc &&
+g++ $AFLAGS -I. "$@" -c satStore.cc  &&
+g++ $AFLAGS -I. "$@" -c wrapper.cc   &&
+ar cqs libabcSat.a satSolver.o satStore.o wrapper.o 
+rm satSolver.o satStore.o wrapper.o
