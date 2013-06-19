ef99b8dfc2d220a7937b743b0d77008e398c0690
Niklas Een <niklas@een.se>
2013-06-19 02:06:15 -0700

    Added MODULE.hh to LutMap

---
+++ LutMap/MODULE.hh
@@ -0,0 +1,9 @@
+#ifndef ZZ__LutMap__LutMap_hh
+#include "ZZ/LutMap/LutMap.hh"
+#endif
+#ifndef ZZ__LutMap__GigReader_hh
+#include "ZZ/LutMap/GigReader.hh"
+#endif
+#ifndef ZZ__LutMap__Cut_hh
+#include "ZZ/LutMap/Cut.hh"
+#endif
