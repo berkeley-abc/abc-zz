dd49280f445580a06396f0d14b427d3cc942ef94
Niklas Een <niklas@een.se>
2013-10-28 21:42:15 -0700

    Sync

---
+++ Gip/IncPdr.hh
@@ -0,0 +1,15 @@
+#ifndef ZZ__Gip__IncPdr_hh
+#define ZZ__Gip__IncPdr_hh
+namespace ZZ {
+using namespace std;
+
+
+//mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+
+void testPdr(Gig& N0);
+
+
+//mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+}
+#endif
