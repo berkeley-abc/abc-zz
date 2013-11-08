d2f750cb94abd1b699331b941eff5d7b4108bd62
Niklas Een <niklas@een.se>
2013-11-07 22:35:37 -0800

    Added command line parsing to FTA

--- Fta/Main_fta.cc
+++ Fta/Main_fta.cc
@@ -1,5 +1,6 @@
 #include "Prelude.hh"
 #include "ZZ_Gig.IO.hh"
+#include "ZZ_CmdLine.hh"
 #include "Parser.hh"
 #include "Solver.hh"
 #include "Solver2.hh"
@@ -17,32 +18,51 @@ int main(int argc, char** argv)
 {
     ZZ_Init;
 
+    cli.add("input", "string", arg_REQUIRED, "Input fault-tree (.tree file).", 0);
+    cli.add("output", "string", "", "Output file.");
+
+    CLI cli_bound;
+    cli_bound.add("dump-cover", "bool", "no", "Dump cover upon complete enumeration.");
+    cli.addCommand("bound", "Compute an upper bound on the probability of the top-node.", &cli_bound);
+
+    cli.addCommand("save-xml", "Save fault-tree in OpenPSA XML format.");
+    cli.addCommand("save-dot", "Save fault-tree in 'dotty' format (no probabilities).");
+
+    cli.parseCmdLine(argc, argv);
+    String input  = cli.get("input").string_val;
+    String output = cli.get("output").string_val;
+
     Gig N;
     Vec<String> ev_names;
     Vec<double> ev_probs;
 
     try{
-        readFaultTree("CEA9601.tree", "CEA9601.prob", N, ev_probs, ev_names);
-//        readFaultTree("baobab1.tree", "baobab1.prob", N, ev_probs, ev_names);       // ~1.68145e-06
-//        readFaultTree("simple2.tree", "simple2.prob", N, ev_probs, ev_names);
-//        readFaultTree("831.tree", "831.prob", N, ev_probs, ev_names);
+        readFaultTree(input, N, ev_probs, ev_names);
     }catch (const Excp_Msg& msg){
         ShoutLn "PARSE ERROR! %_", msg;
         return 1;
     }
-    //**/writeDot("ft.dot", N);
-    //**/writeXml("ft.xml", N, ev_probs, ev_names);
-    //**/exit(0);
 
-    suppress_profile_output = false;
-    setupSignalHandlers();
+    /**/Dump(cli.cmd);
 
-  #if 0
-    WriteLn "Parsed: %_", info(N);
-    enumerateModels(N, ev_probs, ev_names);
-  #else
-    ftaBound(N, ev_probs, ev_names);
-  #endif
+    if (cli.cmd == "save-dot"){
+        if (output == "")
+            output = setExtension(input, "dot");
+        writeDot(output, N);
+        WriteLn "Wrote: \a*%_\a*", output;
+
+    }else if (cli.cmd == "save-xml"){
+        if (output == "")
+            output = setExtension(input, "xml");
+        writeXml(output, N, ev_probs, ev_names);
+        WriteLn "Wrote: \a*%_\a*", output;
+
+    }else{ assert(cli.cmd == "bound");
+        suppress_profile_output = false;
+        setupSignalHandlers();
+
+        ftaBound(N, ev_probs, ev_names);
+    }
 
     return 0;
 }
--- Fta/Parser.cc
+++ Fta/Parser.cc
@@ -23,28 +23,36 @@ using namespace std;
 
 
 // Zero/one-probability events will be eliminated during parsing.
-void readFaultTree(String tree_file, String prob_file, /*outputs:*/Gig& N, Vec<double>& ev_probs, Vec<String>& ev_names)
+void readFaultTree(String tree_file, /*outputs:*/Gig& N, Vec<double>& ev_probs, Vec<String>& ev_names)
 {
     Map<String, GLit> name2gate;
     Vec<char> ftext;    // -- fanin names are stored here
 
+    Str text = readFile(tree_file);
+    if (!text) Throw (Excp_ParseError) "Could not read fault tree from: %_", tree_file;
+
+    uint split = UINT_MAX;
+    for (uint i = 0; i < text.size()-1; i++){
+        if (text[i] == '%' && text[i+1] == '%'){
+            split = i;
+            break; }
+    }
+    if (split == UINT_MAX) Throw (Excp_ParseError) "File does not contain '%%' splitter: %_", tree_file;
+
     // Read probabilities:
     {
-        Str text = readFile(prob_file);
-        if (!text) Throw (Excp_ParseError) "Could not read probabilities from: %_", prob_file;
-
         Vec<Str> lines;
         Vec<Str> fs;
-        strictSplitArray(text, ";", lines);
+        strictSplitArray(text.slice(split + 2), ";", lines);
         for (uint i = 0; i < lines.size(); i++){
             Str s = strip(lines[i]);
             if (s.size() == 0) continue;
 
             splitArray(s, " ", fs);
-            if (fs.size() != 3 || !eq(fs[1], "=")) Throw (Excp_ParseError) "Invalid line in: %_\n  -->> %_", prob_file, s;
+            if (fs.size() != 3 || !eq(fs[1], "=")) Throw (Excp_ParseError) "Invalid line in: %_\n  -->> %_", tree_file, s;
 
             double prob = 0;
-            try{ prob = stringToDouble(fs[2]); }catch (...){ Throw (Excp_ParseError) "Invalid line in: %_\n  -->> %_", prob_file, s; }
+            try{ prob = stringToDouble(fs[2]); }catch (...){ Throw (Excp_ParseError) "Invalid line in: %_\n  -->> %_", tree_file, s; }
 
             Wire w;
             if (prob > 0){
@@ -58,19 +66,15 @@ void readFaultTree(String tree_file, String prob_file, /*outputs:*/Gig& N, Vec<d
                 w = ~N.True();
 
             bool exists = name2gate.set(String(fs[0]), w);
-            if (exists) Throw (Excp_ParseError) "Event '%_' listed twice in: %_", fs[0], prob_file;
+            if (exists) Throw (Excp_ParseError) "Event '%_' listed twice in: %_", fs[0], tree_file;
         }
-        dispose(text);
     }
 
     // Read tree:
     {
-        Str text = readFile(tree_file);
-        if (!text) Throw (Excp_ParseError) "Could not read fault tree from: %_", tree_file;
-
         Vec<Str> lines;
         Vec<Str> fs;
-        strictSplitArray(text, ";", lines);
+        strictSplitArray(text.slice(0, split), ";", lines);
         for (uint i = 0; i < lines.size(); i++){
             // Split line into components:
             Str s = strip(lines[i]);
@@ -103,12 +107,12 @@ void readFaultTree(String tree_file, String prob_file, /*outputs:*/Gig& N, Vec<d
             else if (pfx(gate, "GE_")){
                 w = N.addDyn(gate_CardG, fs.size());
                 uint k = 0;
-                try{ k = stringToUInt64(gate.slice(3)); }catch (...){ Throw (Excp_ParseError) "Invalid line in: %_\n  -->> %_", prob_file, s; }
+                try{ k = stringToUInt64(gate.slice(3)); }catch (...){ Throw (Excp_ParseError) "Invalid line in: %_\n  -->> %_", tree_file, s; }
                 w.arg_set(k);
             }
 
             bool exists = name2gate.set(String(name), w);
-            if (exists) Throw (Excp_ParseError) "Event '%_' listed twice in: %_ + %_", name, tree_file, prob_file;
+            if (exists) Throw (Excp_ParseError) "Event '%_' listed twice in: %_ + %_", name, tree_file, tree_file;
 
             // Store fanin names:
             for (uint i = 0; i < fs.size(); i++){
@@ -133,8 +137,8 @@ void readFaultTree(String tree_file, String prob_file, /*outputs:*/Gig& N, Vec<d
                     Throw (Excp_ParseError) "Missing gate '%_' used as fanin in: %_", name, tree_file;
             }
         }
-        dispose(text);
     }
+    dispose(text);
 
     Add_Gob(N, FanoutCount);
     Wire w_top;
